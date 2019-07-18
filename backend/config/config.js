b3296af508bd9c70eb58c6793332814947bdf683
Barenziah <lunasecrete@gmail.com>
2019-07-18 17:18:23 +0200

    Add environment variables

---
+++ backend/config/config.js
@@ -0,0 +1,24 @@
+/* eslint no-undef: 0 */
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'database_test',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql'
+  }
+};
