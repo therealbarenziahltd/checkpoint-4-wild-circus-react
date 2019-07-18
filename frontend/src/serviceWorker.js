1e2e7d6391780d8a5ef452e62079b048137fa45a
Barenziah <lunasecrete@gmail.com>
2019-07-18 16:39:34 +0200

    Remove console.logs for Travis

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -44,10 +44,6 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
-          );
         });
       } else {
         // Is not localhost. Just register service worker
@@ -72,10 +68,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
+
 
               // Execute callback
               if (config && config.onUpdate) {
@@ -85,7 +78,6 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -122,11 +114,7 @@ function checkValidServiceWorker(swUrl, config) {
         registerValidSW(swUrl, config);
       }
     })
-    .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      );
-    });
+    .catch(() => {});
 }
 
 export function unregister() {
