ac04642d5d2c455326318e24a51f8a81142df58e
Barenziah <lunasecrete@gmail.com>
2019-07-19 14:02:36 +0200

    Add CRUD /users routes

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,9 +1,55 @@
 const User = require('../models').User;
 
 module.exports = {
-  index: function (req, res) {
-    User.findAll()
+  index: (req, res) => {
+    User.scope('withoutPassword').findAll()
       .then((users) => res.json({ users }))
-      .catch((err) => res.send(err));
-  }
+      .catch((err) => res.status(404).send(err));
+  },
+
+  show: (req, res) => {
+    User.scope('withoutPassword').findByPk(req.params.id)
+      .then((user) => user? res.json({ user: user }) : res.status(404).json({error: `User with id ${req.params.id} can't be found.`}))
+      .catch((error) => res.status(503).json({ error: 'Service unavailable' }));
+  },
+
+  create: (req, res) => {
+    User.scope('withoutPassword').create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      isAdmin: req.body.isAdmin || false,
+      birthdate: req.body.birthdate
+    })
+      .then((user) => res.status(201).json({ response: 'User added to database.'}))
+      .catch((error) => res.status(404).json({ error }));
+  },
+
+  update: (req, res) => {
+    User.scope('withoutPassword').findByPk(req.params.id)
+      .then((user) => {
+        user.update({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password,
+          isAdmin: req.body.isAdmin || false,
+          birthdate: req.body.birthdate
+        })
+          .then((user) => res.json({user}))
+          .catch((error) => res.json({error}));
+      })
+      .catch((error) => { res.status(404).json({ error }); });
+  },
+
+  delete: (req, res) => {
+    User.scope('withoutPassword').findByPk(req.params.id)
+      .then((user) => {
+        user.destroy()
+          .then((user) => { res.json({ response: `User ${user.firstName} ${user.lastName} has been deleted.`}); })
+          .catch((error) => res.status(403).json({ error, message: '403 Please contact Admin' }));
+      })
+      .catch((error) => res.status(404).json({ error: '404 User Not Found' }));
+  },
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 const userController = require('../controllers/usersController');
 
-/* GET users listing. */
-router.get('/', userController.index);
+//CREATE (public)
+// ./auth.js
+
+//CREATE (private)
+router.post('/', passport.authenticate('jwt', { session: false }), userController.create);
+
+//READ ALL (private)
+router.get('/', passport.authenticate('jwt', { session: false }), userController.index);
+
+//READ ONE (private)
+router.get('/:id', passport.authenticate('jwt', { session: false }), userController.show);
+
+//UPDATE (private)
+router.put('/:id', passport.authenticate('jwt', { session: false }), userController.update);
+
+//DELETE (private)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), userController.delete);
 
 module.exports = router;
