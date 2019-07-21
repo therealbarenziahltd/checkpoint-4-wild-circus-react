6abd896200c662a21ab2149f7299f1e0607c9582
Barenziah <lunasecrete@gmail.com>
2019-07-21 21:03:45 +0200

    Fix CORS problem

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'public')));
 
